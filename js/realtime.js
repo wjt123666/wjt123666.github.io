4431b3ff4fa709de427dd40331e2ca874d06e4bf
CesteoNew <3566889090@qq.com>
2024-07-20 20:05:27 +0800

    Site updated: 2024-07-20 20:05:27

---
+++ js/realtime.js
@@ -0,0 +1,13 @@
+(() => {
+  dayjs.extend(window.dayjs_plugin_duration)
+  const el = document.getElementById('realtime_duration')
+  // 改成自己的时间
+  const date = dayjs('2024-7-20')
+
+  setInterval(() => {
+    const dur = dayjs.duration(dayjs().diff(date))
+    const days = String(Math.floor(dur.asDays()))
+    el.innerHTML = '已运行' + days + dur.format('天HH时mm分ss秒')
+  }, 1000)
+})()
+
